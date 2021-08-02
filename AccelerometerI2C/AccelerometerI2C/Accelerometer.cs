ab07b103bafd68f85510b36397e582029d10d626
Eunyoung Lee <ey928.lee@samsung.com>
2021-08-02 15:59:35 +0900

    Add I2C sample code

---
+++ AccelerometerI2C/AccelerometerI2C/Accelerometer.cs
@@ -0,0 +1,57 @@
+﻿using Tizen.Applications;
+using Tizen.NUI;
+
+
+namespace AccelerometerI2C
+{
+    public class Accelerometer : NUIApplication
+    {
+        private MainPage mainPage;
+        override protected void OnCreate()
+        {
+            Logger.Debug("");
+
+            base.OnCreate();
+
+            // NOTE To use theme.xaml, uncomment below line.
+            // ThemeManager.ApplyTheme(new Theme(Tizen.Applications.Application.Current.DirectoryInfo.Resource + "theme/theme.xaml"));
+            mainPage = new MainPage();
+
+            GetDefaultWindow().Add(mainPage);
+            GetDefaultWindow().KeyEvent += OnKeyEvent;
+        }
+
+        private void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            Logger.Debug("");
+
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+        override protected void OnPause()
+        {
+            base.OnPause();
+        }
+
+        override protected void OnResume()
+        {
+            base.OnResume();
+        }
+
+        override protected void OnTerminate()
+        {
+            Logger.Debug("");
+
+            mainPage.DataUpdateStop();
+            base.OnTerminate();
+        }
+
+        override protected void OnAppControlReceived(AppControlReceivedEventArgs e)
+        {
+            base.OnAppControlReceived(e);
+        }
+    }
+}
---
+++ AccelerometerI2C/AccelerometerI2C/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AccelerometerI2C
+{
+    public static class Logger
+    {
+        private const string LOGTAG = "Accel";
+        public static void Debug(string message, [CallerFilePath] string file = "", [CallerMemberName] string func = "", [CallerLineNumber] int line = 0)
+        {
+            global::Tizen.Log.Debug(LOGTAG, message, file, func, line);
+        }
+        public static void Error(string message, [CallerFilePath] string file = "", [CallerMemberName] string func = "", [CallerLineNumber] int line = 0)
+        {
+            global::Tizen.Log.Error(LOGTAG, message, file, func, line);
+
+        }
+    }
+}
