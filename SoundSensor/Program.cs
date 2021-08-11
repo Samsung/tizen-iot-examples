821d1f6ac013e5b99e2415dbb9d92f1cd0c89dfe
kiso.chang <kiso.chang@samsung.com>
2021-08-11 14:17:22 +0900

    Add SoundSensor project
    - example for test Peripheral SPI C# Api
    
    Signed-off-by: kiso.chang <kiso.chang@samsung.com>

---
+++ SoundSensor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tizen.NUI;
+
+namespace SoundSensor
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Scene1 Instance = new Scene1();
+            Instance.Run(args);
+        }
+    }
+}
---
+++ SoundSensor/Scene1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tizen.Applications;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Xaml;
+
+namespace SoundSensor
+{
+    public class Scene1 : NUIApplication
+    {
+        override protected void OnCreate()
+        {
+            base.OnCreate();
+
+            // NOTE To use theme.xaml, uncomment below line.
+            // ThemeManager.ApplyTheme(new Theme(Tizen.Applications.Application.Current.DirectoryInfo.Resource + "theme/theme.xaml"));
+
+            GetDefaultWindow().Add(new Scene1Page());
+            GetDefaultWindow().KeyEvent += OnScene1KeyEvent;
+        }
+
+        private void OnScene1KeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+        override protected void OnPause()
+        {
+            base.OnPause();
+        }
+
+        override protected void OnResume()
+        {
+            base.OnResume();
+        }
+
+        override protected void OnTerminate()
+        {
+            base.OnTerminate();
+        }
+
+        override protected void OnAppControlReceived(AppControlReceivedEventArgs e)
+        {
+            base.OnAppControlReceived(e);
+        }
+    }
+}
